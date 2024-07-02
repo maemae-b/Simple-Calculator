cdc2f20ab5c2b39e7170192d55cfe359cc77018a
maemae-b <filipinasjessiellemae@gmail.com>
2024-07-02 16:17:17 +0800

    Create basic variables

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let num1 = 0;
+let num2 = 0;
+let sum = 0;
+let operation;
+
+// DOM
+const EXPRESSION = document.querySelector("#expression");
+const INPUT = document.querySelector("#input");
+
+// map buttons so they correspond to display
+const ALL_BTNS = Array.from(document.querySelectorAll("button"));
